3c1462b595ac5d25fa3d347bf85ed1149fdf61a6
MakicLin <makiclin@gmail.com>
2018-09-26 23:06:37 -0700

    Create flowery-lawnmower.js

---
+++ flowery-lawnmower.js
@@ -0,0 +1,40 @@
+function setup() {
+  createCanvas(400, 200);
+  colorMode(HSB, 360, 100, 100, 100);
+}
+
+var x = 0;
+var h = 10;
+
+function draw() {
+  stroke(random(60, 70), 100, 90);
+  line(x, height-10, x + random(-10, 10), height-10-random(h));
+  
+  x = x + 10;
+  
+  if (x > width) {
+    x = random(10);
+    h = h + 5;
+  }
+  
+  if (random() > 0.999) {
+    noStroke();
+    fill(255);
+    rect(-1, -1, width+2, height-15);
+    h = 10;
+  }
+  
+  noStroke();
+  fill(40, 100, 60);
+  rect(0, height-10, width, 10);
+  
+  if (h > 60) {
+  fill(156, 55, 94)
+  translate(120, 140);
+  noStroke();
+  for (var i = 0; i < 10; i ++) {
+    ellipse(0, 10, 4, 10);
+    rotate(PI/5);
+  }
+    
+  }}
